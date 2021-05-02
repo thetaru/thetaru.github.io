709e3875e85f4ba176be3f4d18b95421468baf37
thetaru <sensesensesense012345678910@gmail.com>
2021-05-02 15:26:02 +0900

    modify

--- src/components/typing.js
+++ src/components/typing.js
@@ -2,23 +2,24 @@ import React, { useEffect } from 'react'
 import styles from './typing.module.css';
 
 const type = (element, sentence) => {
-    [...sentence].forEach((character, index) => {
+    let message = Array.from(sentence);
+    message.forEach((character, index) => {
         setTimeout(() => {
             document.querySelector(element).textContent += character;
-        }, 100 * ++index);
+        }, 100 * (index + 1));
     });
 }
 
 const Typing = () => {
-    let message = 'Welcome to my homepage';
+    let message = 'Welcome to my homepage'
     useEffect(() => {
-        type("#typing", message);
+        type('#typing', message);
     }, [message]);
     return (
         <header>
             <div className="banner">
                 <div className="banner-text">
-                    <h1 id="typing"></h1>
+                    <h1 id='typing'></h1>
                 </div>
             </div>
         </header>
